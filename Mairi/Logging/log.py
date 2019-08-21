4fea13d1a756823e15ac6ab6bcd8b7c979bf7f3b
xxx <1026737316@qq.com>
2019-08-21 18:38:42 +0800

    nothing

--- Mairi/Logging/log.py
+++ Mairi/Logging/log.py
@@ -8,25 +8,6 @@ myformat = '%(asctime)s---%(levelname)s-[line:%(lineno)d]---"message":%(message)
 timename = time.strftime("%Y-%m-%d-%I-%M-%S", time.localtime())+"-MairiLog"
 filename = r'F:\Program\Logging\yuetu\%s.txt' % timename
 
-class Mylog():
-
-
-    def __init__(self,name):
-        timename = time.strftime("%Y-%m-%d-%I-%M-%S", time.localtime()) + "-mairilog"
-        self.path = r'F:\Program\Logging\yuetu\%s.txt' % timename
-        self.name = name
-        self.logger = logging.getLogger(self.name)
-        self.logger.setLevel(logging.INFO)
-        self.ch = logging.StreamHandler()
-        gs = logging.Formatter('%(asctime)s---%(levelname)s-[line:%(lineno)d]---"message":%(message)s')
-        self.ch.setLevel(gs)
-        self.fh = logging.handlers.TimedRotatingFileHandler
-        self.format = logging
-        pass
-
-
-
-
 logging.basicConfig(
     #设置告警级别为INFO
     #自定义打印的格式
@@ -42,3 +23,18 @@ logging.basicConfig(
 logging.info("这是一个info的log")
 logging.error("这是一个error的log")
 logging.info("这又是一个info的log")
+
+class Mylog():
+
+    def __init__(self,name):
+        timename = time.strftime("%Y-%m-%d-%I-%M-%S", time.localtime()) + "-mairilog"
+        self.path = r'F:\Program\Logging\yuetu\%s.txt' % timename
+        self.name = name
+        self.logger = logging.getLogger(self.name)
+        self.logger.setLevel(logging.INFO)
+        self.ch = logging.StreamHandler()
+        gs = logging.Formatter('%(asctime)s---%(levelname)s-[line:%(lineno)d]---"message":%(message)s')
+        self.ch.setLevel(gs)
+        self.fh = logging.handlers.TimedRotatingFileHandler
+        self.format = logging
+        pass
