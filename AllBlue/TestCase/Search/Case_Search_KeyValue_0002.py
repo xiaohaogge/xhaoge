de527143aa227f1465a23efea964252d04fc61a2
xxx <1026737316@qq.com>
2019-09-02 18:50:03 +0800

    add

--- AllBlue/TestCase/Search/Case_Search_KeyValue_0002.py
+++ AllBlue/TestCase/Search/Case_Search_KeyValue_0002.py
@@ -12,11 +12,11 @@ class Case_Search_KeyValue_0002(CaseBase):
 
     def __init__(self):
         CaseBase.__init__(self)
-        self.log.info("Case_Search_KeyValue_0002,测试开始")
+        self.log.info("===Case_Search_KeyValue_0002,测试开始===")
 
 
     def TestProcess(self):
-        self.log.info('Case_Search_KeyValue_0002,进入测试步骤！')
+        self.log.info('===Case_Search_KeyValue_0002,进入测试步骤！===')
 
         self.log.info('测试ctrip的本位币,请求币种是CNY,本位币应该是USD')
         self.Test_Master_Currency(plat='ctrip', reqC='CNY', MstCurrecy='CNY')
@@ -36,12 +36,12 @@ class Case_Search_KeyValue_0002(CaseBase):
 
 
     def TestResult(self):
-        self.log.info('Case_Search_KeyValue_0002,测试完毕')
+        self.log.info('===Case_Search_KeyValue_0002,测试完毕===')
         if self.flag:
             self.log.info('Case_Search_KeyValue_0002,测试通过')
             print("测试结果很成功，perfect！")
         else:
-            self.log.info('Case_Search_KeyValue_0002,测试失败')
+            self.log.info('【Case_Search_KeyValue_0002,测试失败】')
 
     # 定义公共方法，用于判断masterCuurrncy；
     def Test_Master_Currency(self,plat='ctrip',reqC='CNY',MstCurrecy='CNY'):
