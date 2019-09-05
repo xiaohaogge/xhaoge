3a552aefb3447cb75d41773b10fe3f4c0e33b6a9
nima <1026737316@qq.com>
2019-09-05 23:10:42 +0800

    add cnrrency

--- AllBlue/TestCase/Currency/Case_Currency_FromToRate_0001.py
+++ AllBlue/TestCase/Currency/Case_Currency_FromToRate_0001.py
@@ -18,19 +18,37 @@ class Case_Currency_FromToRate_0001(CaseBase):
         self.log.info('===Case_Search_KeyValue_0002,进入测试步骤！===')
 
         self.log.info('测试ctrip的sscts,从USD到CNY的汇率是否拿到')
-        self.Test_Currency()
+        self.Test_Currency(pro='sscts', cid='ctrip', ori="USD", tar='CNY')
 
         self.log.info('测试ctrip的sscts,从TWD到CNY的汇率是否拿到')
-        self.Test_Currency(ori='TWD')
+        self.Test_Currency(pro='sscts', cid='ctrip', ori="TWD", tar='CNY')
 
+        self.log.info('测试ctrip的ttnet,从EUR到CNY的汇率是否拿到')
+        self.Test_Currency(pro='ttnet', cid='ctrip', ori="EUR", tar='CNY')
 
-        self.log.info('测试iwofly的本位币,请求币种是USD,本位币应该是USD')
+        self.log.info('测试ctrip的ssgmt,从THB到CNY的汇率是否拿到')
+        self.Test_Currency(pro='ssgmt', cid='ctrip', ori="THB", tar='CNY')
 
+        self.log.info('测试ctrip的ssxm,从HKD到CNY的汇率是否拿到')
+        self.Test_Currency(pro='ssgmt', cid='ctrip', ori="HKD", tar='CNY')
 
-        self.log.info('测试iwofly的本位币,请求币种是HKD,本位币应该是HKD')
+        self.log.info('测试ctrip的belair,从INR到CNY的汇率是否拿到')
+        self.Test_Currency(pro='belair', cid='ctrip', ori="INR", tar='CNY')
 
+        self.log.info('测试qunarytb的avia,从RUB到CNY的汇率是否拿到')
+        self.Test_Currency(pro='avia', cid='qunarytb', ori="RUB", tar='CNY')
 
-        self.log.info('测试iwoflyCN的本位币,请求币种是CNY,本位币应该是CNY')
+        self.log.info('测试ctrip的via,从SGD到CNY的汇率是否拿到')
+        self.Test_Currency(pro='via', cid='ctrip', ori="SGD", tar='CNY')
+
+        self.log.info('测试ctrip的ssact,从AUD到CNY的汇率是否拿到')
+        self.Test_Currency(pro='ssact', cid='ctrip', ori="AUD", tar='CNY')
+
+        self.log.info('测试ctrip的ssjdc,从CAD到CNY的汇率是否拿到')
+        self.Test_Currency(pro='ssjdc', cid='ctrip', ori="CAD", tar='CNY')
+
+        self.log.info('测试ctrip的sscts,从VND到CNY的汇率是否拿到')
+        self.Test_Currency(pro='sscts', cid='ctrip', ori="VND", tar='CNY')
 
         self.flag = True
 
@@ -47,7 +65,7 @@ class Case_Currency_FromToRate_0001(CaseBase):
     def Test_Currency(self,pro='sscts',cid='ctrip',ori="USD",tar='CNY'):
         strJoin = "?providerName={}&cid={}&originalCode={}&targetCode={}".format(pro,cid,ori,tar)
         sendUrl = self.PreProdRate+strJoin
-        print(sendUrl)
+        # print(sendUrl)
         # pre-prod-restful-api.gloryholiday.com/nightking/exchangeRate?providerName=sscts&cid=ctrip&originalCode=USD&targetCode=CNY
         resjson = self.sendRequest(method='GET',url=sendUrl)
         resdict = json.loads(resjson)
