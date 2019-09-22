42041754abf299257af1fad89b39d2be1cbea7d2
nima <1026737316@qq.com>
2019-09-22 23:30:34 +0800

    add read casexml

---
+++ AllBlue/TestCase/CaseCommon.py
@@ -0,0 +1,42 @@
+''' Case Common
+    定义case 中使用的公共函数，以供调用；
+'''
+
+from AllBlue.Common.NightKingSearchResponse import NightKingSearchRes
+
+# search case
+def Test_RepoRule(res='', provider=''):
+    '''定义获取repoIds 是否为空，result 值为true：repoIds不为空；值为false，repoIds为空；'''
+    providerRoutings = Test_FindProviderRouting(res1=res, pro=provider)
+    result = True
+    for r in providerRoutings:
+        if len(r['rule']['repoRule']['repoIds']) == 0:
+            result = False
+    return result
+
+
+def Test_FindProviderRouting(res1='', pro=''):
+    '''定义查找某一provider的所有航线报价'''
+    resp = NightKingSearchRes(res1)
+    routings = resp.nkRouting
+    if pro == '':
+        return routings
+    providerList = []
+    for i in routings:
+        if i['providerName'] == pro:
+            providerList.append(i)
+
+    if len(providerList) == 0:
+        raise Exception('not found provider routing,please check!')
+    return providerList
+
+# verify case
+
+
+
+
+# order case
+
+
+
+# currency case
