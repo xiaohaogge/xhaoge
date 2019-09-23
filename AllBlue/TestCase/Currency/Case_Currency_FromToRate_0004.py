011b973e2be3238fbad1eb9134cc2512a371c845
nima <1026737316@qq.com>
2019-09-23 22:59:10 +0800

    add c4

--- AllBlue/TestCase/Currency/Case_Currency_FromToRate_0004.py
+++ AllBlue/TestCase/Currency/Case_Currency_FromToRate_0004.py
@@ -20,13 +20,14 @@ class Case_Currency_FromToRate_0004(CaseBase):
 
         self.log.info('【1.测试从night king中返回获取不同的provider(以平台qunarytb为例)】')
         res = self.sendRequest(method='POST',url=self.nkRequesturl,data=self.nkRequestdata)
+        print(res)
         self.checkNkStatus(res)
         self.target_providers = self.Test_TargetProviders(res)
         self.log.info('target_provider:%s'%self.target_providers)
 
-        self.log.info('【2.根据不同的供应商,可能匹配不同的政策，政策内的币种可能会不一样；】')
+        self.log.info('【2.根据不同的供应商,rule当中涉及币种可能会不一样；】')
         for tar in self.target_providers:
-            self.Test_PolicyChange(provider=tar,routings=self.routingslist)
+            self.Test_RuleChange(provider=tar,routings=self.routingslist)
 
         self.log.info('【3.测试从night king中返回获取provider币种(以iwoflyCOM为例)】')
         self.nkRequestDataDict['Cid'] = 'iwoflyCOM'
@@ -51,7 +52,7 @@ class Case_Currency_FromToRate_0004(CaseBase):
             self.log.info('=========Case_Currency_FromToRate_0004,测试失败')
 
 
-    def Test_PolicyChange(self,cid='',provider='',routings=''):
+    def Test_RuleChange(self,cid='',provider='',routings=''):
         pro_Routing_List = []
         for d in routings:
             if d['providerName'] == provider:
@@ -63,55 +64,32 @@ class Case_Currency_FromToRate_0004(CaseBase):
         testnum = random.randint(0, num - 1)
         self.log.info('%s总航线数目：%s,选择的是：%s' % (provider,num, testnum))
         test_Routing = pro_Routing_List[testnum]
-        pro_Currency = test_Routing['providerCurrency']
+        Rule_List_Currency = []
+        Rule_All = test_Routing['rule']
+        origin_Rule = Rule_All['originRuleFee']['currency']
+        Rule_List_Currency.append(origin_Rule) if origin_Rule is not None else ''
+        try:
+            manual_Rule = Rule_All['manualFareRule']['currency']
+        except Exception:
+            manual_Rule = 0
+        Rule_List_Currency.append(manual_Rule) if manual_Rule !=0 else ''
+
+        try:
+            Rule_By_Policy = Rule_All['changeDetail']['ruleChangeByPolicy']['currency']
+        except Exception:
+            Rule_By_Policy = 0
+        Rule_List_Currency.append(Rule_By_Policy) if Rule_By_Policy != 0 else ''
+
         mas_Currency = test_Routing['masterCurrency']
         out_Currency = test_Routing['currency']
         cuyconversions = test_Routing['currencyConversions']
-        policy_Price_Changes = test_Routing['policyPriceChanges']
-        self.log.info('【2.1.check %s是否有获取到policyPriceChanges】' % provider)
-        if len(policy_Price_Changes)!=0:
-            policy_Currency_List = self.Test_GetPolicyCurrecy(masterC=mas_Currency,policy=policy_Price_Changes)
-            for i in policy_Currency_List:
+
+        self.log.info('【2.1.check %s 的rule中是否有获取到Currency】' % provider)
+        if len(Rule_List_Currency)!=0:
+            for i in Rule_List_Currency:
                 if i != mas_Currency:
                     result = self.getRoutingCurrencyConvs(conversions=cuyconversions,fromC=i,toC=mas_Currency)
                     if result:
                         self.log.info('政策中from %s to %s 有获取到汇率'%(i,mas_Currency))
                     else:
                         self.log.error('政策中from %s to %s 没有获取到汇率'%(i,mas_Currency))
-
-
-
-    def Test_GetPolicyCurrecy(self,masterC='CNY',policy=''):
-        '''定义获取policyPriceChanges 里的需要的from to的币种转换 return list'''
-        po_Currency_List = []
-        for c in policy:
-            if c['adultChange']['totalChange']['currency']!=masterC:
-                return '本位币需要check,masterCurrency:%s'%masterC
-            try:
-                addC = c['adultChange']['absChange']['currency']
-                po_Currency_List.append(addC)
-                if c['policyType']=='TICKET_FEE' or c['policyType']=='PLATFORM':
-                    '''顺便check一下 婴儿加价'''
-                    if not c.has_key("infantChange"):
-                        self.log.error('%s政策中不存在婴儿计算'%c['policyType'])
-            except Exception:
-                pass
-        return po_Currency_List
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
