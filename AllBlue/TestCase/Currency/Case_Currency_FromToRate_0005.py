651d4b3850e5b6848741a23b0b062c1b685b0c0b
nima <1026737316@qq.com>
2019-10-06 18:20:44 +0800

    add c5

--- AllBlue/TestCase/Currency/Case_Currency_FromToRate_0005.py
+++ AllBlue/TestCase/Currency/Case_Currency_FromToRate_0005.py
@@ -1,10 +1,8 @@
 '''Currency Rate
   此case用于测试nightking search 时返回的供应商原始价格，经过政策最后报出去的价格，是否计算无误；
 '''
-
 import json
 import random
-from AllBlue.TestCase.CaseBase.CommonFunc import CheckListOnly
 from AllBlue.TestCase.CaseBase.SearchCaseBase import CaseBase
 
 
@@ -27,7 +25,7 @@ class Case_Currency_FromToRate_0005(CaseBase):
 
         self.log.info('【1.1.根据不同的供应商,来进行不同的原始-对外报价计算；】')
         for tar in self.target_providers:
-            self.Test_RuleChange(provider=tar,routings=self.routingslist)
+            self.Test_CountPrice(provider=tar,routings=self.routingslist)
 
         self.log.info('【2.测试从night king中返回获取provider币种(以iwoflyCOM,请求为CNY为例)】')
         self.nkRequestDataDict['Cid'] = 'iwoflyCOM'
@@ -37,9 +35,9 @@ class Case_Currency_FromToRate_0005(CaseBase):
         self.checkNkStatus(res)
         self.target_providers = self.Test_TargetProviders(res=res)
         self.log.info('target_provider:%s' % self.target_providers)
-        self.log.info('【2.1.根据不同的供应商,rule当中涉及币种可能会不一样；】')
+        self.log.info('【2.1.根据不同的供应商,来进行不同的原始-对外报价计算；】')
         for tar in self.target_providers:
-            self.Test_RuleChange(provider=tar,routings=self.routingslist)
+            self.Test_CountPrice(provider=tar,routings=self.routingslist)
 
         self.log.info('【3.测试从night king中返回获取provider币种(以iwoflyCOM,请求为USD为例)】')
         self.nkRequestDataDict['Currency'] = 'USD'
@@ -49,9 +47,9 @@ class Case_Currency_FromToRate_0005(CaseBase):
         self.checkNkStatus(res)
         self.target_providers = self.Test_TargetProviders(res=res)
         self.log.info('target_provider:%s' % self.target_providers)
-        self.log.info('【3.1.根据不同的供应商,rule当中涉及币种可能会不一样；】')
+        self.log.info('【3.1.根据不同的供应商,来进行不同的原始-对外报价计算；】')
         for tar in self.target_providers:
-            self.Test_RuleChange(provider=tar, routings=self.routingslist)
+            self.Test_CountPrice(provider=tar, routings=self.routingslist)
 
         self.log.info('【4.测试从night king中返回获取provider币种(以iwoflyCOM,请求为HKD为例)】')
         self.nkRequestDataDict['Currency'] = 'HKD'
@@ -61,9 +59,9 @@ class Case_Currency_FromToRate_0005(CaseBase):
         self.checkNkStatus(res)
         self.target_providers = self.Test_TargetProviders(res=res)
         self.log.info('target_provider:%s' % self.target_providers)
-        self.log.info('【4.1.根据不同的供应商,rule当中涉及币种可能会不一样；】')
+        self.log.info('【4.1.根据不同的供应商,来进行不同的原始-对外报价计算；】')
         for tar in self.target_providers:
-            self.Test_RuleChange(provider=tar, routings=self.routingslist)
+            self.Test_CountPrice(provider=tar, routings=self.routingslist)
 
         self.flag = True
 
@@ -75,6 +73,7 @@ class Case_Currency_FromToRate_0005(CaseBase):
             print("测试结果很成功，perfect！")
         else:
             self.log.error('=========Case_Currency_FromToRate_0005,测试失败')
+            print("测试结果很失败，so bad！")
 
 
     def Test_CountPrice(self,provider='',routings=''):
@@ -87,28 +86,48 @@ class Case_Currency_FromToRate_0005(CaseBase):
             return self.log.info('该供应商没有航线报出:%s'%provider)
         '''随机抽取其中一条航线，进行测试计算；'''
         testnum = random.randint(0, num-1)
-        self.log.info('%s总航线数目：%s,选择的是：%s进行测试；' % (provider,num, testnum))
+        self.log.info('%s总航线数目:%s,选择的是第:%s条航线进行测试；' % (provider,num, testnum))
         testRouting = pro_Routing_List[testnum]
         self.log.info('先获取供应商原始价格，再去拿到每个政策中的加价值；')
-        policy_Adult_Amount = []
-        policy_Child_Amount = []
         oriAult_Price = testRouting['oriAdultPrice']
         oriAdult_Tax = testRouting['oriAdultTax']
         oriChild_Price = testRouting['oriChildPrice']
         oriChild_Tax = testRouting['oriChildPrice']
-
-
         mas_Currency = testRouting['masterCurrency']
         out_Currency = testRouting['currency']
-        cuyconversions = testRouting['currencyConversions']
+        pro_Currency = testRouting['providerCurrency']
+        curconversions = testRouting['currencyConversions']
+        policyChanges = testRouting['policyPriceChanges']
+        adultPrice = testRouting['adultPrice']
+        self.log.info('policyChanges:%s；'%policyChanges)
+        if pro_Currency != mas_Currency:
+            rate,source,idply = self.getRoutingCurrencyConvs(method=2,conversions=curconversions,
+                                                             fromC=pro_Currency,toC=mas_Currency)
+            oriAult_Price = oriAult_Price * rate
 
-        self.log.info('【check[%s]的rule中是否有获取到Currency】' % provider)
-        if len(Rule_List_Currency)!=0:
-            Rule_List_Currency = CheckListOnly(dataList=Rule_List_Currency)
-            for i in Rule_List_Currency:
-                if i != mas_Currency:
-                    result,rate = self.getRoutingCurrencyConvs(conversions=cuyconversions,fromC=i,toC=mas_Currency)
-                    if result:
-                        self.log.info('政策中from %s to %s 有获取到汇率:%s'%(i,mas_Currency,rate))
-                    else:
-                        self.log.error('政策中from %s to %s 没有获取到汇率,错误：%s'%(i,mas_Currency,test_Routing))
+        amountMasCur = self.Test_CountAdultPrice(oriprice=oriAult_Price,policyList=policyChanges)
+        amountOutCur = amountMasCur
+        if mas_Currency != out_Currency:
+            rate,source,idply = self.getRoutingCurrencyConvs(method=2,conversions=curconversions,
+                                                             fromC=mas_Currency,toC=out_Currency)
+            amountOutCur = amountMasCur * rate
+        self.log.info('原始价格(币种:%s):%d，本位币报价(本位币:%s):%d，计算对外报价(报价币:%s):%d；'%(pro_Currency,oriAult_Price,
+                                                                   mas_Currency,amountMasCur,out_Currency,amountOutCur))
+        result = adultPrice-amountOutCur
+        print('ori:', oriAult_Price, 'amountMasCur', amountMasCur)
+        if result >2 or result < -2:
+            raise Exception('计算值%s和对外报价值%s差值%s大于2，请检查；'%(amountOutCur,adultPrice,result))
+
+        self.log.info('计算值%s和对外报价值%s差值%s；'%(amountOutCur,adultPrice,result))
+
+
+    def Test_CountAdultPrice(self,oriprice='',policyList=[]):
+        for i in policyList:
+            try:
+                pri = i['adultChange']['totalChange']['amount']
+                display = i['displayNumber']
+                self.log.info('%s policy 加价值:%d' %(display,pri))
+                oriprice += pri
+            except Exception:
+                pass
+        return oriprice
