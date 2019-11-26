7a8156127220479e418b2433fb13a3c9805ea224
xxx <1026737316@qq.com>
2019-11-26 13:46:12 +0800

    add order

--- AllBlue/TestCase/Order/Case_Order_KeyValue_0001.py
+++ AllBlue/TestCase/Order/Case_Order_KeyValue_0001.py
@@ -9,9 +9,12 @@ class Case_Order_KeyValue_0001(CaseBase):
 
 
     def TestProcess(self):
-        searchRouting = self.search()
-
-
+    	# 选择测试平台进行搜索，对返回的不能进行verify的航线 进行筛选；
+        searchAllRouting = self.Search(cid="qunarytb",TripType="1",FromCity="BJS",ToCity="SIN",FromDate="20200223",
+        	RetDate="20200521",AdultNumber=1,ChildNumber=0,InfatNumber=0)
+        ableVerifyRouting = self.verifyAbleRoutings(searchAllRouting["routing"])
+        pidVerifyRouting = self.verifyByPid(routings=ableVerifyRouting)
+        respVerify = self.Verify(cid="qunarytb",adultNum=1,childNum=0,infantNum=0,currency="CNY",routing=pidVerifyRouting)
 
 
     def TestResult(self):
--- Mairi/Test/CNiwoflyPolicy.py
+++ Mairi/Test/CNiwoflyPolicy.py
@@ -1,18 +1,16 @@
 
 import requests
 import json
-import math
 
 # 例如B端的价格为X，Y 为搜索时需要加的费用，
 # 公式为：（X+Y）*（Commission的百分比+预收支付成本的百分比）+（Commission的数值金额+预收支付成本的数值金额）=Y
 # Xp 票面价 Xt 税费；searchC 搜索币种，payC 支付币种；numP 人数
 numP = 1
-searchC = "KRW"
-payC = "KRW"
-Xp = 233320
-Xt = 234858
+searchC = "CNY"
+payC = "USD"
+Xp = 2639
+Xt = 991
 X = Xp+Xt  
-# 1014  5050.62677
 # ===================platform policy============= 
 # currency1 = from政策 commission数值币种 
 # currency2 = from政策 预收支付数值币种
@@ -37,12 +35,12 @@ payP = 0.0431
 orderP = 0
 orderV = 0
 currency4 = "USD"
-ratelossP = 0.017
+ratelossP = 0
 ratelossV = 0
 currency5 = "USD"
 
 def getcurrencyrate(fromC="USD",toC="USD"):
-	url = "http://dev-restful-api.gloryholiday.com/nightking/exchangeRate?providerName=%22%22&cid=iwoflyCOM&originalCode={}&targetCode={}".format(fromC,toC)
+	url = "http://test-restful-api.gloryholiday.com/nightking/exchangeRate?providerName=%22%22&cid=iwoflyCOM&originalCode={}&targetCode={}".format(fromC,toC)
 	res = requests.get(url=url)
 	rr = res.json()
 	rate = rr["exchange_rate"]["exchange_rate"]
@@ -62,7 +60,7 @@ def countCurrencyPolicy():
 	# 最终search 对外报价；
 	platformFinal = x1 + Y
 	print("-------------------------------------")
-	print("币种政策加价值为：",math.ceil(Y),"\n") 
+	print("平台政策加价值为：",round(Y,2),"\n") 
 	print("最终对外报价总计：",round(platformFinal,2),payC,"\n")
 	print("-----------server policy-------------")
 	orderV1 = orderV *getcurrencyrate(fromC=currency4,toC=payC)
